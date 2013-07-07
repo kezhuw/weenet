2394dc88bb42d73de252a495a6e72a569ee57561
Kezhu Wang <kezhuw@gmail.com>
2013-07-07 21:47:19 +0800

    fix weenet.h for client usage

--- src/weenet.h
+++ src/weenet.h
@@ -1,12 +1,14 @@
 #ifndef __WEENET_INCLUDE_H_
 #define __WEENET_INCLUDE_H_
 
-#include <atom.h>
-#include <atomic.h>
-#include <event.h>
-#include <logger.h>
-#include <memory.h>
-#include <process.h>
-#include <service.h>
+#include "atom.h"
+#include "atomic.h"
+#include "event.h"
+#include "logger.h"
+#include "memory.h"
+#include "process.h"
+#include "service.h"
+#include "timer.h"
+#include "types.h"
 
 #endif
