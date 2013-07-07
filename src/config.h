46713fbe10484f73c4f5cf6aeaeeff4817b0e897
Kezhu Wang <kezhuw@gmail.com>
2013-07-07 19:52:14 +0800

    fix a typo and add some configs in config.h

--- src/config.h
+++ src/config.h
@@ -3,8 +3,10 @@
 
 #define WEENET_DEFAULT_SERVICE_PATH	"/usr/lib/weenet/services/?.so;/usr/share/weenet/services/?.so"
 
-#define WEENET_DEFUALT_CONFIG_FILE		"/etc/weenet.conf"
+#define WEENET_DEFAULT_CONFIG_FILE	"/etc/weenet.conf"
 
 #define WEENET_DEFAULT_LOGGER_DIR	"/var/log/weenet"
+
+#define WEENET_DEFAULT_THREADS		8
 
 #endif
